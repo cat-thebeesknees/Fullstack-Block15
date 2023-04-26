e8210d97727c61cc7312fbc5fb6de58023793ef0
Catherine Hilton <catherine.hilton1786@gmail.com>
2023-04-26 17:23:02 -0400

    completed step one of the homework assignment. Wrote psuedocode for step two. Next complete step two, and write psuedocode for step three.

---
+++ froyo.js
@@ -0,0 +1,21 @@
+//create an object and name it "customer"
+const customer = {
+    //list all the keys and then add the value for each one
+    firstName: "jake",
+    lastName: "smith",
+    email: "jaekSmih!aol.com",
+    phone:"",
+    zipCode: "631",
+    favoriteFlavors: 32,
+    cupSize: "medium",
+    favoriteStore: "Target",
+    firstVisit: "false",
+    }
+    //update the values using bracket notation for:
+    //correct the information for email to "Jak3Smith1992@email.com"
+    
+    //correct the information phone to 3195551234
+    
+    //correct the information zipCode to "63132"
+    
+    //correct the information favoriteFlavors to "coffee", "strawberry", and "matcha"
