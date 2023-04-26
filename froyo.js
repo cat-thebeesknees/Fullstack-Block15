1ca2dbfd435d8147af71bb07c6eb81fdcdb5aebc
Catherine Hilton <catherine.hilton1786@gmail.com>
2023-04-26 19:00:24 -0400

    completed step two, next will do psuedocode for step three

--- froyo.js
+++ froyo.js
@@ -10,13 +10,18 @@ const customer = {
     cupSize: "medium",
     favoriteStore: "Target",
     firstVisit: "false",
-    }
-
+    };
+console.log(customer);
     //Step 2- update the values using bracket notation for:
     //correct the information for email to "Jak3Smith1992@email.com"
-    
+    customer["email"] = "Jak3Smith1992@email.com";
+    console.table(customer);
     //correct the information phone to 3195551234
-    
+    customer["phone"] = "3195551234";
+    console.log(customer);
     //correct the information zipCode to "63132"
-    
+    customer["zipCode"] = "63132";
+    console.table(customer);
     //correct the information favoriteFlavors to "coffee", "strawberry", and "matcha"
+    customer["favoriteFlavors"] = ["coffee", "strawberry", "matcha"];
+    console.log(customer);
